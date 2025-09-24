262c57665543b3ccc0ca592bc62fbe935c01fdf1
Artem Raskin <artem.rosh17@gmail.com>
2025-09-23 17:30:16 -0700

    Set up create and insert sql files, and make sql requests

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+    ('Mr. Chocolate', 8, 'M', 'brown', 'calm', 1), 
+    ('Rowdy', 5, 'M', 'black', 'rowdy', 1),
+    ('Tabitha', 4, 'F', 'black', 'friendly', 1),
+    ('Sergeant Brown', 9, 'M', 'brown', 'serious', 1),
+    ('Melissa', 7, 'F', 'white', 'gentle', 1),
+    ('Grinch', 6, 'M', 'green', 'grumpy', 0), 
+    ('Wendy', 3, 'F', 'brown', 'sweet', 1),
+    (NULL, 2, 'F', 'gray', 'shy', 1);
